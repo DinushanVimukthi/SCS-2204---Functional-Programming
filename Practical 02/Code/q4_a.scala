4b6056a7035dfc0f3112188e9023954640bed8cb
Dinushan Vimukthi <stdinushan@gmail.com>
2022-07-10 23:26:12 +0530

    Refined Practical 2

--- Practical 02/Code/q4_a.scala
+++ Practical 02/Code/q4_a.scala
@@ -9,6 +9,8 @@ def Run():Unit={
     val tax=Tax(Normal_Hours,OT_Hours)
     println(Salary(pureSalary,tax));
 }
-def PureSalary(Normal_Hours:Int,OTHours:Int):Int = {250*Normal_Hours + 85*OTHours}
+def PureSalary(Normal_Hours:Int,OTHours:Int):Int = (250*Normal_Hours + 85*OTHours)
 def Tax(NormalHrs:Int,OTHrs:Int):Float=if(NormalHrs>=40 && OTHrs>=30) 0.12 else 0
-def Salary(PureSalary:Int,Tax:Float):Float={PureSalary-PureSalary*Tax}
+def Salary(PureSalary:Int,Tax:Float):Float=(PureSalary-PureSalary*Tax)
+
+
--- Practical 02/Code/q4_b.scala
+++ Practical 02/Code/q4_b.scala
@@ -1,21 +1,7 @@
 @main
-def run()={
-   prg(5)
-   prg(10)
-   prg(15)
-   prg(20)
-}
-def prg(tp:Int)= println(profit(revenue(tp),cost(tp)))
-def profit(revenue:Int,cost:Int)=revenue-cost
-def cost(tp:Int):Int={500+3*attend(tp)}
-def revenue(tp:Int)=attend(tp)*tp
-def attend(tp:Int):Int={
-    if(tp==15)
-        120
-    else if(tp>15)
-        120+((tp-15)/5)*20
-    else if(tp<15)
-        120-((15-tp)/5)*20
-    else
-        0
-}
+def run():Unit=printf("Best Price iis Rs.%d",bestPrice(5))
+def bestPrice(tp:Int):Int=if(profit(tp)>profit(tp+5)) tp else bestPrice(tp+5)
+def profit(tp:Int):Int=revenue(tp)-cost(tp)
+def cost(tp:Int):Int=(500+3*attend(tp))
+def revenue(tp:Int):Int=attend(tp)*tp
+def attend(tp:Int):Int=(120+(15-tp)*4)
