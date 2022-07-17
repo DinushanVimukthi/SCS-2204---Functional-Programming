430573b6fc9527ef6d74d694e0f6e05b1193c6cf
Dinushan Vimukthi <stdinushan@gmail.com>
2022-07-17 23:27:07 +0530

    Edited⭕

--- Practical 03/Tutorial3.scala
+++ Practical 03/Tutorial3.scala
@@ -7,13 +7,11 @@ def run():Unit={
     printf("4. Total wholesale cost for 60 copies Rs.%.2f\n",TotalWholeSaleCost(60))
     printf("5. Total Runiing Time %d min\n",TotalRuniingTime())
 }
-
-
 def AreaOfDisk(Radius:Double):Double=Radius*Radius*Pi
 def CelciusToFarenheit(Celcius:Double):Double=Celcius*9/5+32
 def VolumeOfSphere(Radius:Double):Double=4/3*Pi*Radius*Radius*Radius
 def TotalWholeSaleCost(Quantity:Int):Double=Quantity match {
-    case Quantity if Quantity>50 => 3*50 + (Quantity-50)*0.75 +24.95*Quantity*0.4
-    case _ => Quantity*3 +24.95*Quantity*0.4
+    case Quantity if Quantity>50 => 3*50 + (Quantity-50)*0.75 +24.95*Quantity*0.6
+    case _ => Quantity*3 +24.95*Quantity*0.6
 }
 def TotalRuniingTime(easy:Int=8,tempo:Int=7):Int=2*easy+3*tempo+2*easy
